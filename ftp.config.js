425fd9833d9b5e8aa331fc1a4e2f6aa243002c83
hubary <han.liu10@pactera.com>
2021-09-03 18:16:04 +0800

    ftp.config.js修改

--- ftp.config.js
+++ ftp.config.js
@@ -4,43 +4,30 @@
  * @配置多台机器
  **/
 module.exports = [
-  // {
-  //   name: 'project-1-xxx', // 项目1
-  //   ssh: {
-  //     host: '10.114.14.65',
-  //     port: 22,
-  //     username: 'test',
-  //     password: '123456',
-  //   },
-  //   romotePath: '/home/front/dist', // 远程地址
-  //   localPath: './dist', // 本地地址
-  //   rootDir: 'dist',
-  //   isTest: true, // 是否开启测试模式
-  // },
-  // {
-  //   name: 'project-2-xxx', // 项目2
-  //   ssh: {
-  //     host: '10.114.14.110',
-  //     port: 22,
-  //     username: 'test',
-  //     password: '123456',
-  //   },
-  //   romotePath: '/home/front/dist', // 远程地址
-  //   localPath: './dist', // 本地地址
-  //   rootDir: 'dist',
-  //   isTest: true, // 是否开启测试模式
-  // },
   {
-    name: '租户管理-10.114.14.65-nginx', // 项目/服务器名称
+    name: 'project-1-xxx', // 项目1
     ssh: {
       host: '10.114.14.65',
       port: 22,
-      username: 'esb',
-      password: 'Esb#2021',
+      username: 'test',
+      password: '123456',
     },
-    romotePath: '/home/esb/front/dist-tenant2', // 远程地址
-    localPath: './dist-tenant2', // 本地地址
-    rootDir: 'dist-tenant2',
-    isTest: false,
+    romotePath: '/home/front/dist', // 远程地址
+    localPath: './dist', // 本地地址
+    rootDir: 'dist',
+    isTest: true, // 是否开启测试模式
+  },
+  {
+    name: 'project-2-xxx', // 项目2
+    ssh: {
+      host: '10.114.14.110',
+      port: 22,
+      username: 'test',
+      password: '123456',
+    },
+    romotePath: '/home/front/dist', // 远程地址
+    localPath: './dist', // 本地地址
+    rootDir: 'dist',
+    isTest: true, // 是否开启测试模式
   },
 ];
